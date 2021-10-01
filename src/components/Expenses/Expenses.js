efc6e5a9a7a987a1bd488be84b8fb80cd746b560
Strik3ria <robert_coones@zohomail.com>
2021-10-01 10:08:35 -0500

    End of section 3

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const { expenses } = props;
+  return (
+    <Card className="expenses">
+      {expenses.map((e) => (
+        <ExpenseItem title={e.title} amount={e.amount} date={e.date} />
+      ))}
+    </Card>
+  );
+};
+
+export default Expenses;
